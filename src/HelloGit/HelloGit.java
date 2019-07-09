b34f36ff555c567d7782a3a8d1a71e8d85227ec5
peter <549338875@qq.com>
2019-07-09 16:13:18 +0800

    commit Hello IDEA!

--- src/HelloGit/HelloGit.java
+++ src/HelloGit/HelloGit.java
@@ -3,5 +3,6 @@ package HelloGit;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("HelloGit!");
+        System.out.println("Hello IDEA!");
     }
 }
